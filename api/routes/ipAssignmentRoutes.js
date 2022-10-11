9d2e5e5f8af88737ed7380479a5c637dab4b996d
Allen Vanegas Diaz <avanegasdiaz@langara.ca>
2022-10-11 16:02:10 -0700

    Added root route

--- api/routes/ipAssignmentRoutes.js
+++ api/routes/ipAssignmentRoutes.js
@@ -3,6 +3,9 @@ module.exports = function(app) {
   var controller = require('../controllers/ipAssignmentController');
 
   // IP Assignment Routes
+  app.route('/').get((req, res) => {
+    res.send('TDx IP Assignment API')
+  })
   app.route('/ip')
     .post(controller.ipAssignmentController);
 };
