27e1c227b50457428d5e2354fea60332a764a81c
Allen Vanegas Diaz <avanegasdiaz@langara.ca>
2022-10-11 17:56:08 -0700

    Fixing tdapi for Heroku

--- api/controllers/ipAssignmentController.js
+++ api/controllers/ipAssignmentController.js
@@ -2,7 +2,6 @@ const fetch = require('node-fetch');
 
 exports.ipAssignmentController = function(req, serverRes) {
     console.log("Logging into TDx");
-    fetch()
 
     const TDAPI = require('tdapi');
     // Authenticate
